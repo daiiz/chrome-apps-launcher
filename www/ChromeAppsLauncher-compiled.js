5602211953fc05f0db689925594b85f83c970505
daiiz <da.iz.mg@gmail.com>
2016-07-02 01:24:47 +0900

    拡張機能に進む

---
+++ www/ChromeAppsLauncher-compiled.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+/**
+ * Created by daiki on 2016/07/02.
+ */
+
+var ChromeAppsLauncher = function ChromeAppsLauncher() {
+  _classCallCheck(this, ChromeAppsLauncher);
+};
+
+//# sourceMappingURL=ChromeAppsLauncher-compiled.js.map
---
+++ www/ChromeAppsLauncher.js
@@ -0,0 +1,8 @@
+/**
+ * Created by daiki on 2016/07/02.
+ */
+class ChromeAppsLauncher {
+    constructor () {
+
+    }
+}
